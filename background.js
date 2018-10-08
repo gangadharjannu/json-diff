59b86e8cfa3ab779ff521aceb390ed52f6a96527
gangadhar <gangadhara.jannu@ing.nl>
2018-10-08 16:26:02 +0200

    remoced commented code

--- background.js
+++ background.js
@@ -1,20 +1,8 @@
-// chrome.browserAction.onClicked.addListener(function (activeTab) {
-//     chrome.tabs.create({
-//         url: chrome.extension.getURL('index.html')
-//     }, function (tab) {
-//         // TODO: tab opened
-//     });
-// });
 chrome.browserAction.onClicked.addListener(function (tab) {
     chrome.tabs.create({
         'url': chrome.extension.getURL('jdd/index.html'),
-        // 'active': true
+    }, function (tab) {
+        // TODO: tab opened
+
     });
 });
-
-// chrome.browserAction.onClicked.addListener(function (activeTab) {
-//     var newURL = "http://stackoverflow.com/";
-//     chrome.tabs.create({
-//         url: newURL
-//     });
-// });
